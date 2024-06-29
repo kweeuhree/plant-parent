d2792e5ad92092d879c691614871bcac6b584e70
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-29 10:08:42 -0400

    add plant api routes

---
+++ src/app/api/plants/[id].ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import connectToDb from '../../lib/connectToDb';
+import Plant from "../../models/Plant";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  await connectToDb();
+
+  switch (method) {
+    case "GET" /* Get a model by its ID */:
+      try {
+        const plant = await Plant.findById(id);
+        if (!plant) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: plant });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "PUT" /* Edit a model by its ID */:
+      try {
+        const plant = await Plant.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        if (!plant) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: plant });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "DELETE" /* Delete a model by its ID */:
+      try {
+        const deletedPlant = await Plant.deleteOne({ _id: id });
+        if (!deletedPlant) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
