82959c67d655c1045967154c404f3306a6cc6512
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-29 11:01:19 -0400

    add main point of entry

---
+++ src/app/index.tsx
@@ -0,0 +1,42 @@
+import connectToDb from '../app/lib/connectToDb';
+import User, { Users } from "../../src/app/models/User";
+import { GetServerSideProps } from "next";
+
+type Props = {
+    pets: Users[];
+  };
+  
+  const Index = ({ users }: Props) => {
+    return (
+      <>
+        {users.map((user) => (
+          <div key={user._id}>
+            <div className="card">
+              <img src={user.image_url} />
+              <h5 className="pet-name">{user.name}</h5>
+              <div className="main-content">
+                <p className="pet-name">{user.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  };
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    await connectToDb();
+  
+    /* find all the data in our database */
+    const result = await User.find({});
+  
+    /* Ensures all objectIds and nested objectIds are serialized as JSON data */
+    const users = result.map((doc) => {
+      const user = JSON.parse(JSON.stringify(doc));
+      return user;
+    });
+  
+    return { props: { users: users } };
+  };
+
+  export default Index;
