299c04c4ac170704ee6670365015f0ae9cd83b4e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-29 10:11:03 -0400

    add user api routes

---
+++ src/app/api/users/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import connectToDb from '../../lib/connectToDb';
+import User from "../../models/User";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { method } = req;
+
+  await connectToDb();
+
+  switch (method) {
+    case "GET":
+      try {
+        const users = await User.find({}); /* find all the data in our database */
+        res.status(200).json({ success: true, data: users });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const user = await User.create(
+          req.body,
+        ); /* create a new model in the database */
+        res.status(201).json({ success: true, data: user });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
