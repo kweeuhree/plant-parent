0ec6b5f2d56da0f3685b2d02a56839e41c91b2de
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-29 09:41:26 -0400

    connect to db logic

---
+++ src/app/lib/connectToDb.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+declare global {
+  var mongoose: any; // This must be a `var` and not a `let / const`
+}
+
+const DB_URL = process.env.DB_URL;
+
+if (!DB_URL) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDb() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(DB_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connectToDb;
