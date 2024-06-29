b647abcab7d07295620ab64a43e7289da1ba5c96
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-29 09:08:50 -0400

    fix typeerror related to mongodb bson parser

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// update nextConfig to fix "TypeError: Cannot read properties of undefined (reading 'prototype')"
+// This issue is caused by this change in MongoDB's bson parser. 
+// MongoDB's bson parser uses top-level await and dynamic import in ESM mode to avoid some Webpack bundling issues. 
+// And Next.js forces ESM mode.
+
+const nextConfig = {
+    experimental: {
+      esmExternals: "loose", // <-- add this
+      serverComponentsExternalPackages: ["mongoose"] // <-- and this
+    },
+    // and the following to enable top-level await support for Webpack
+    webpack: (config) => {
+      config.experiments = {
+        topLevelAwait: true
+      };
+      return config;
+    },
+  }
 
 export default nextConfig;
