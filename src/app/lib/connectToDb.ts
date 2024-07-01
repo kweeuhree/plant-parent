98c04330e5d6a60c1360daf1084848c6bc2176ba
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-01 16:09:56 -0400

    updated mongodb uri

--- src/app/lib/connectToDb.ts
+++ src/app/lib/connectToDb.ts
@@ -1,46 +1,66 @@
-import mongoose from "mongoose";
+import { connect } from "http2";
 import dotenv from "dotenv";
 
+import mongoose from "mongoose";
 dotenv.config();
 
-declare global {
-  var mongoose: any; // This must be a `var` and not a `let / const`
-}
-
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local",
-  );
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectToDb() {
-  if (cached.conn) {
-    return cached.conn;
+const connectToDb = async () => {
+  if(mongoose.connections[0].readyState) {
+    return true;
   }
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
+
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log('konnekted to kluster');
+    return true;
+  } catch (error) {
+    console.log(error);
   }
-
-  return cached.conn;
 }
 
 export default connectToDb;
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// declare global {
+//   var mongoose: any; // This must be a `var` and not a `let / const`
+// }
+
+// const MONGODB_URI = process.env.MONGODB_URI as string;
+
+// if (!MONGODB_URI) {
+//   throw new Error(
+//     "Please define the MONGODB_URI environment variable inside .env.local",
+//   );
+// }
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function connectToDb() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+//   if (!cached.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
+//     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+//       return mongoose;
+//     });
+//   }
+//   try {
+//     cached.conn = await cached.promise;
+//   } catch (e) {
+//     cached.promise = null;
+//     throw e;
+//   }
+
+//   return cached.conn;
+// }
+
+// export default connectToDb;
