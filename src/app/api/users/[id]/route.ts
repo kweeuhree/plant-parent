6e8f6cdb2e578606dbc5f9e79306e03274ee645f
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-03 07:44:11 -0400

    dynamic route for user api DELETE

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -7,7 +7,7 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const id = params.id // 'a', 'b', or 'c'
+  const id = params.id 
   console.log(id);
 
   const user = await User.findById(id);
@@ -18,20 +18,49 @@ export async function GET(
   return NextResponse.json({ success: true, data: user}, { status: 200});
 }
 
+// Handle PUT user requests
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id 
+  console.log(id);
 
+  try {
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!User) {
+      return NextResponse.json({ success: false}, { status: 400});
+      }
 
-// export async function oiET(req: NextRequest, res: NextResponse) {
+    return NextResponse.json({ success: true, data: user}, { status: 200});
 
-//   const { id } = req.query;
-//   try {
-//     await connectToDb();
-//     const users = await User.find({});
-//     return NextResponse.json({ success: true, data: users });
-//   } catch (error) {
-//     console.error('Error fetching users:', error);
-//     return NextResponse.json({ success: false, message: 'Error fetching users' }, { status: 400 });
-//   }
-// }
+    } catch (error) {
+    return NextResponse.json({ success: false}, { status: 400});
+  }
+}
+
+// Handle DELETE user requests
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id // 'a', 'b', or 'c'
+  console.log(id);
+
+  try {
+    const deletedUser = await User.deleteOne({ _id: id });
+    if (!deletedUser) {
+      return NextResponse.json({ success: false}, { status: 400});
+    }
+    return NextResponse.json({ success: true, message: 'User delelted'}, { status: 200});
+
+  } catch (error) {
+    return NextResponse.json({ success: false}, { status: 400});
+  }
+}
 
 // export default async function handler(
 //   req: NextApiRequest,
@@ -57,31 +86,31 @@ export async function GET(
   //     }
   //     break;
 
-//     case "PUT" /* Edit a model by its ID */:
-//       try {
-//         const user = await User.findByIdAndUpdate(id, req.body, {
-//           new: true,
-//           runValidators: true,
-//         });
-//         if (!User) {
-//           return res.status(400).json({ success: false });
-//         }
-//         res.status(200).json({ success: true, data: user });
-//       } catch (error) {
-//         res.status(400).json({ success: false });
-//       }
-//       break;
+    // case "PUT" /* Edit a model by its ID */:
+    //   try {
+    //     const user = await User.findByIdAndUpdate(id, req.body, {
+    //       new: true,
+    //       runValidators: true,
+    //     });
+    //     if (!User) {
+    //       return res.status(400).json({ success: false });
+    //     }
+    //     res.status(200).json({ success: true, data: user });
+    //   } catch (error) {
+    //     res.status(400).json({ success: false });
+    //   }
+    //   break;
 
 //     case "DELETE" /* Delete a model by its ID */:
-//       try {
-//         const deletedUser = await User.deleteOne({ _id: id });
-//         if (!deletedUser) {
-//           return res.status(400).json({ success: false });
-//         }
-//         res.status(200).json({ success: true, data: {} });
-//       } catch (error) {
-//         res.status(400).json({ success: false });
-//       }
+      // try {
+      //   const deletedUser = await User.deleteOne({ _id: id });
+      //   if (!deletedUser) {
+      //     return res.status(400).json({ success: false });
+      //   }
+      //   res.status(200).json({ success: true, data: {} });
+      // } catch (error) {
+      //   res.status(400).json({ success: false });
+      // }
 //       break;
 
 //     default:
