363695b74822386a62b00d86b0a54c853dee763e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-03 09:14:34 -0400

    edit console.log messages

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { connectToDb } from '../../../lib/connectToDb';
-import User from '../../../models/User';
+import { connectToDb } from '../../../(lib)/connectToDb';
+import User from '../../../(models)/User';
 
 // Handle GET specific user requests
 export async function GET(
@@ -34,7 +34,7 @@ export async function PUT(
       // runValidators: true,
     });
     if (!User) {
-      return NextResponse.json({ success: false, message: 'User with this id isnt found'}, { status: 400});
+      return NextResponse.json({ success: false, message: 'User with this id not found'}, { status: 400});
       }
 
     return NextResponse.json({ success: true, data: user}, { status: 200});
@@ -57,66 +57,9 @@ export async function DELETE(
     if (!deletedUser) {
       return NextResponse.json({ success: false}, { status: 400});
     }
-    return NextResponse.json({ success: true, message: 'User delelted'}, { status: 200});
+    return NextResponse.json({ success: true, message: 'User deleted'}, { status: 200});
 
   } catch (error) {
     return NextResponse.json({ success: false}, { status: 400});
   }
 }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse,
-// ) {
-//   const {
-//     query: { id },
-//     method,
-//   } = req;
-
-//   await connectToDb();
-
-  // switch (method) {
-  //   case "GET" /* Get a model by its ID */:
-  //     try {
-  //       const user = await User.findById(id);
-  //       if (!user) {
-  //         return res.status(400).json({ success: false });
-  //       }
-  //       res.status(200).json({ success: true, data: user });
-  //     } catch (error) {
-  //       res.status(400).json({ success: false });
-  //     }
-  //     break;
-
-    // case "PUT" /* Edit a model by its ID */:
-    //   try {
-    //     const user = await User.findByIdAndUpdate(id, req.body, {
-    //       new: true,
-    //       runValidators: true,
-    //     });
-    //     if (!User) {
-    //       return res.status(400).json({ success: false });
-    //     }
-    //     res.status(200).json({ success: true, data: user });
-    //   } catch (error) {
-    //     res.status(400).json({ success: false });
-    //   }
-    //   break;
-
-//     case "DELETE" /* Delete a model by its ID */:
-      // try {
-      //   const deletedUser = await User.deleteOne({ _id: id });
-      //   if (!deletedUser) {
-      //     return res.status(400).json({ success: false });
-      //   }
-      //   res.status(200).json({ success: true, data: {} });
-      // } catch (error) {
-      //   res.status(400).json({ success: false });
-      // }
-//       break;
-
-//     default:
-//       res.status(400).json({ success: false });
-//       break;
-//   }
-// }
