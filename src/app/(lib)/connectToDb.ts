512874c256bf1633e57ba0b2f417af6b64cb55cf
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-10 15:53:17 -0400

    toubleshoot deployment

--- src/app/(lib)/connectToDb.ts
+++ src/app/(lib)/connectToDb.ts
@@ -21,17 +21,21 @@ if (!cached) {
 }
 
 export async function connectToDb() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
+    if (cached.conn) {
+        return cached.conn;
+      }
+      if (!cached.promise) {
+        const opts = {
+          bufferCommands: false,
+        };
+        cached.promise = mongoose.connect(process.env.MONGODB_URI as string, opts).then((mongoose) => {
+          console.log('MongoDB connected');
+          return mongoose;
+        }).catch((err) => {
+          console.error('MongoDB connection error:', err);
+          throw err;
+        });
+      }
   try {
     cached.conn = await cached.promise;
   } catch (e) {
