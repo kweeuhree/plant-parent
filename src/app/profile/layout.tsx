6f7d2781b758bb6c4de1a3153724c37f20e0d24e
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-11 16:28:50 -0400

    troubleshoot deployment

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -3,7 +3,7 @@
 import { Inter } from "next/font/google";
 // import "./globals.css";
 // import SideBar
-import SideBar from "../../(components)/SideBar/SideBar";
+import SideBar from "../(components)/SideBar/SideBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
